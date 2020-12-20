ee0e8063f5d616a227c8d23271b50f5d35825a31
bsdharshi <bsdharshi@gmail.com>
2020-12-20 20:55:08 +0530

    Problem 2

---
+++ W3 resource exercise answers- part-1/Two.java
@@ -0,0 +1,14 @@
+//Write a Java program to print the sum of two numbers
+//Write a Java program to print the sum of two numbers
+
+import java.util.*;
+public class Two {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter number 1:");
+        int number1 = sc.nextInt();
+        System.out.print("Enter number 2:");
+        int number2 = sc.nextInt();
+        System.out.println("Sum of two number is: "+(number1+number2));
+    }
+}
