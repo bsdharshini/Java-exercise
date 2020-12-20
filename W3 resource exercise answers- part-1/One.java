38dbce6b12233d88dcde679cfc65d75c03188eed
bsdharshi <bsdharshi@gmail.com>
2020-12-20 20:43:42 +0530

    First problem

---
+++ W3 resource exercise answers- part-1/One.java
@@ -0,0 +1,8 @@
+// 1) Write a Java program to print 'Hello' on screen and then print your name on a separate line
+class One
+{
+public static void main(String[] args) {
+    System.out.println("Hello \nDharshini");
+}
+}
+//getting name from user
---
+++ W3 resource exercise answers- part-1/One1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class One1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("First Name:");
+        String fname = sc.next();
+        System.out.print("Last Name:");
+        String lname = sc.next();
+        System.out.println("Hello "+fname+" "+lname+"!");
+        
+    }
+    
+}
