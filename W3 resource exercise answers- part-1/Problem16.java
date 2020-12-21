024c55e68de37d2d428c612058bd468723f94aa2
bsdharshi <bsdharshi@gmail.com>
2020-12-21 12:00:28 +0530

    Problem 16

---
+++ W3 resource exercise answers- part-1/Problem16.java
@@ -0,0 +1,16 @@
+/*Write a Java program to print a face
+ +"""""+                                                 
+[| o o |]                                                
+ |  ^  |                                                 
+ | '-' |                                                 
+ +-----+
+*/
+public class Problem16 {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
